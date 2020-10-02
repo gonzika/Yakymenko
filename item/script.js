be1b8184f5f9ebb69577e1df9557a3e0749c87f1
gonzika <hziben@gmail.com>
2020-10-02 12:21:18 +0300

    20/10/02

--- item/script.js
+++ item/script.js
@@ -15,4 +15,25 @@ async function myFunc() {
     }
 }
 
+// function translate() {
+//     title = "Resume Yakymenko Mykola"
+//     name = "Yakymenko Mykola"
+//     tel = "Mobile-phone:"
+//     birthday = "Birthday: 1991/10/31"
+//     city = "City: Mariupol, Ukraine"
+//     education = "Education"
+//     account = "01.09.2011 - 21.06.2015. Priazovsky State Technical University. Specialty: accounting and audit. Scientific degree: Master"
+//     works = "My works"
+//     emTitle = "Don't finished yet"
 
+//     document.getElementsByTagName("TITLE")[0].text = title
+//     document.getElementById('name').innerHTML = name
+//     document.getElementById('tel').innerHTML = tel
+//     document.getElementById('birthday').innerHTML = birthday
+//     document.getElementById('city').innerHTML = city
+//     document.getElementById('education').innerHTML = education
+//     document.getElementById('account').innerHTML = account
+//     document.getElementById('works').innerHTML = works
+//     document.getElementById('today').title = emTitle
+//     document.getElementById('today1').title = emTitle
+// }
